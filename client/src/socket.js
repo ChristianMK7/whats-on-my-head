fbcd00f8b59ae80a75f42e31de796be8eb8e8e5f
ChristianMK7 <birdkingk@gmail.com>
2025-04-15 17:11:51 +0300

    🔧 Hardcoded backend URL in socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,5 @@
-// socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://whats-on-my-head-server.onrender.com"); // Your deployed backend
 
 export default socket;
